baa8cdab08becca0bf348eea294447b321f08dba
NurRizqiMaulana <maulana29.rizqi@gmail.com>
2023-12-15 22:48:44 +0700

    Add SplashScreen guys

---
+++ app/src/main/java/com/dicoding/spicifyapplication/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.spicifyapplication.ui.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.spicifyapplication.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
